9ccb77f454c5a63039aacf09b446a5869dde106a
Su Khai <khai_373@hotmail.com>
2014-10-06 18:24:14 -0700

    update

--- src/Cryptosystem.java
+++ src/Cryptosystem.java
@@ -23,6 +23,20 @@ import javax.swing.text.DefaultEditorKit;
  * tool and configuration for the maximum value for the keys used in the
  * program.
  * 
+ * This program was influenced from the book, Discrete Mathematics and Its 
+ * Applications 7th Edition (Chapter 4 Number Theory and Cryptography. This
+ * program can only encrypt alphabetic characters, and it encrypts two
+ * characters at once. The resulting cipher text will be in the format of only
+ * numbers.
+ * 
+ * Alphabetic to number conversion for this program:
+ * A: 00    G: 06    M: 12    S: 18    Y: 24
+ * B: 01    H: 07    N: 13    T: 19    Z: 25
+ * C: 02    I: 08    O: 14    U: 20
+ * D: 03    J: 09    P: 15    V: 21
+ * E: 04    K: 10    Q: 16    W: 22
+ * F: 05    L: 11    R: 17    X: 23
+ * 
  * @author Su Khai Koh
  */
 public class Cryptosystem extends JFrame {
